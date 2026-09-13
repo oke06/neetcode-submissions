0f5d3238becb33420c8f5db04f5805d810ce105c
Oke Boadu <150081561+oke06@users.noreply.github.com>
2026-09-13 16:30:55 +0100

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pairs = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in pairs:
+                return [pairs[diff], i]
+            pairs[n] = i
+        return False
+            
